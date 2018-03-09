d3ca3eedd4a578e41bd39c588929b5fddd100d0a
S. Scott Whitmore <steven-whitmore@uiowa.edu>
2018-03-09 17:45:30 -0600

    Fixed dependency on global object in expand_surfaces_to_volume.

--- R_to_incorporate/expand_surfaces_to_volume.R
+++ R_to_incorporate/expand_surfaces_to_volume.R
@@ -1,25 +1,41 @@
 expand_surfaces_to_volume <- function(surface_array, vol_dim) {
-  n_voxels <- 
-    vol_dim %>% 
-    cumprod() %>% 
+  n_voxels <-
+    vol_dim %>%
+    cumprod() %>%
     max()
-  
+
   n_surfaces <- dim(surface_array)[3]
-  
-  surface_array_padded <- create_seg_array(example$segmentation)
-  
+
+  surface_array_padded <-
+    abind::abind(
+      array(rep(0, vol_dim[1] * vol_dim[2]), dim = vol_dim[1:2]),
+      surface_array,
+      array(rep(vol_dim[3], vol_dim[1] * vol_dim[2]), dim = vol_dim[1:2]),
+      along = 3
+    )
+    # array(
+    #   c(
+    #     rep(0, vol_dim[1] * vol_dim[2]),
+    #     values_ordered,
+    #     rep(vol_dim[3], vol_dim[1] * vol_dim[2])
+    #   ),
+    #   dim = c(vol_dim[1],
+    #           vol_dim[2],
+    #           n_surfaces + 2)
+    # )
+
   result_vol <- array(rep(NA, n_voxels), dim = vol_dim)
-  
-  layer_thicknesses <- 
-    surface_array_padded[ , , c(2:13)] - 
-    surface_array_padded[ , , c(1:12)] 
-  
+
+  layer_thicknesses <-
+    surface_array_padded[ , , c(2:(n_surfaces + 2))] -
+    surface_array_padded[ , , c(1:(n_surfaces + 1))]
+
   for(i in 1:dim(result_vol)[1]) {
     for(j in 1:dim(result_vol)[2]) {
-      
+
       lengths <- layer_thicknesses[i, j, ]
       values <- 0:n_surfaces
-      
+
       result_vol[i, j, ] <-
         inverse.rle(
           list(
@@ -29,6 +45,6 @@ expand_surfaces_to_volume <- function(surface_array, vol_dim) {
         )
     }
   }
-  
+
   result_vol
 }
