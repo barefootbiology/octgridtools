e1e43176c51a08e66e194e887b18c9a5b8cfcea7
S. Scott Whitmore <steven-whitmore@uiowa.edu>
2018-03-27 08:48:28 -0500

    Added the 'read_oct_files' function to package.

--- R/read_oct_files.R
+++ R/read_oct_files.R
@@ -1,32 +1,34 @@
-library(heyexr)
-
-# TASK: Extend this function to work with all the files produced by OCTExplorer.
-
-read_oct_files <- function(volume = NA, 
-                           segmentation = NA, 
+#' Read files related to one OCT image
+#'
+#' Reads a set of files produced by OCTExplorer related to a single OCT image.
+#'
+#' @export
+#' @importFrom heyexr read_heyexr read_segmentation_xml read_center_xml
+read_oct_files <- function(volume = NA,
+                           segmentation = NA,
                            grid_center = NA) {
   result <- list()
-  
+
   if(is.na(volume) && is.na(segmentation) && is.na(grid_center)) {
     error("Provide at least one file path for volume, segmentation, or grid_center.")
   }
-  
+
   if(!is.na(volume)) {
     message("Reading volume file ", volume)
     result$volume <- read_heyex(vol_file = volume)
   }
-  
+
   if(!is.na(segmentation)) {
     message("Reading segmentation file ", segmentation)
     result$segmentation <- read_segmentation_xml(xml_file = segmentation)
   }
-  
+
   if(!is.na(grid_center)) {
     message("Reading grid center file ", grid_center)
-    
+
     result$grid_center <- read_center_xml(center_file = grid_center)
   }
-  
+
   message(length(result), " file(s) successfully read.")
   result
 }
