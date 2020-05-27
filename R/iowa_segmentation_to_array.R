8cc358c3dddda584d6432ed1dffbdae4b5a9d71c
S. Scott Whitmore <steven-whitmore@uiowa.edu>
2020-05-27 08:24:40 -0500

    Add option to mask undefined segmentation surfaces when converting segmentation from data.frame to array.

--- R/iowa_segmentation_to_array.R
+++ R/iowa_segmentation_to_array.R
@@ -2,13 +2,27 @@
 #'
 #' Convert Iowa segmentation data.frame to an array.
 #' Produces an array in the shape of:
-#' dim(# ascans, # bscans, #surface_id)
+#' dim(# ascans, # bscans, # surface_id)
 #'
 #' @export
 #' @importFrom dplyr select
 #' @importFrom reshape2 acast
-iowa_segmentation_to_array <- function(segmentation) {
-  segmentation$layers %>%
+iowa_segmentation_to_array <- function(segmentation, mask_undefined = FALSE) {
+  seg_layers <- segmentation$layers
+
+  seg_undefined <-
+    segmentation$undefined_region %>%
+    mutate(is_defined = FALSE)
+
+  if(mask_undefined) {
+    seg_layers <-
+      seg_layers %>%
+      left_join(seg_undefined) %>%
+      mutate(is_defined = if_else(is.na(is_defined), TRUE, FALSE)) %>%
+      mutate(value = if_else(is_defined, value, as.numeric(NA)))
+  }
+
+  seg_layers %>%
     select(ascan_id, bscan_id, surface_id, value) %>%
     acast(formula = ascan_id ~ bscan_id ~ surface_id)
 }
