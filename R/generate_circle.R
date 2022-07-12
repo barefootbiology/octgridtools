dbf17384ca767ab035d3fb86a810140cfaa11489
S. Scott Whitmore <steven-whitmore@uiowa.edu>
2022-07-12 13:50:32 -0500

    Remove importFrom no longer needed.

--- R/generate_circle.R
+++ R/generate_circle.R
@@ -2,7 +2,6 @@
 #'
 #'
 #' @export
-#' @importFrom tibble tibble
 generate_circle <- function(center = c(0, 0), radius = 1, n = 100) {
   angle <- seq(0, 2 * pi, length = n)
   x <- center[1] + radius * cos(angle)
