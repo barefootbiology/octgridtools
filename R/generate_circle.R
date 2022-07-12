86335abab630d06939018f2f3393eaca64c537e6
S. Scott Whitmore <steven-whitmore@uiowa.edu>
2022-07-12 13:48:42 -0500

    Revise code to be independent of source.

--- R/generate_circle.R
+++ R/generate_circle.R
@@ -1,14 +1,12 @@
 #' Create a circle
 #'
-#' Code based on:
-#' http://gastonsanchez.com/blog/how-to/2012/06/17/PCA-in-R.html
 #'
 #' @export
 #' @importFrom tibble tibble
-generate_circle <- function(center = c(0, 0), radius = 1, npoints = 100) {
-  tt <- seq(0, 2 * pi, length = npoints)
-  xx <- center[1] + radius * cos(tt)
-  yy <- center[2] + radius * sin(tt)
+generate_circle <- function(center = c(0, 0), radius = 1, n = 100) {
+  angle <- seq(0, 2 * pi, length = n)
+  x <- center[1] + radius * cos(angle)
+  y <- center[2] + radius * sin(angle)
 
-  tibble(x = xx, y = yy)
+  data.frame(x = x, y = y)
 }
