93777b1ce16bebbc39aea6232604e12a88fecf4e
S. Scott Whitmore <steven-whitmore@uiowa.edu>
2022-07-13 14:23:10 -0500

    Fixed spelling error.

--- R/unique_regions.R
+++ R/unique_regions.R
@@ -1,6 +1,6 @@
 #' Identify the unique region IDs in a segmentation volume
 #'
-#' Identifies the unique region IDs in a segmetnation volume.
+#' Identifies the unique region IDs in a segmentation volume.
 #'
 #' @export
 unique_regions <- function(seg_volume) {
