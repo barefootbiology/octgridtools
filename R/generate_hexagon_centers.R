e7d5ecbbea7a21c7a48f055b4672fd8e5449671f
S. Scott Whitmore <steven-whitmore@uiowa.edu>
2022-07-13 14:22:58 -0500

    Modified code to make it distinct.

--- R/generate_hexagon_centers.R
+++ R/generate_hexagon_centers.R
@@ -5,13 +5,13 @@
 #' @export
 #' @importFrom dplyr rowwise mutate ungroup
 #' @importFrom magrittr %>%
-generate_hexagon_centers <- function(nrow, ncol, radius, x0=0, y0=0) {
+generate_hexagon_centers <- function(nrow, ncol, radius, x0 = 0, y0 = 0) {
   expand.grid(1:nrow, 1:ncol) %>%
     setNames(c("row_id", "col_id")) %>%
     rowwise() %>%
     mutate(
-      center_x = .calc_x(i = row_id, j = col_id, radius, x0 = x0),
-      center_y = .calc_y(i = row_id, j = col_id, radius, y0 = y0)
+      center_x = .calc_x(row = row_id, col = col_id, radius, x0 = x0),
+      center_y = .calc_y(row = row_id, col = col_id, radius, y0 = y0)
     ) %>%
     ungroup() %>%
     mutate(.cell_id = 1:n())
--- R/helper_functions.R
+++ R/helper_functions.R
@@ -1,14 +1,19 @@
 #' Given an rows, columns, and radius, determine the grid of hexagon centers
 #'
-#' Code from:
+#' Code adapted from:
 #' http://hacksociety.net/Thread-Tutorial-Creating-a-hexagonal-grid-for-games-C
+#' which doesn't seem to be on the Internet anymore.
 #' @keywords internal
-.calc_y <- function(i, j, s, y0=0) {
-  a <- sqrt(3) * (s / 2)
+.calc_y <- function(row, col, radius, y0 = 0) {
+  a <- sqrt(3) * (radius / 2)
 
-  y0 + (j %% 2) * a + 2 * i * a
+  # Starting from the y origin,
+  # if the column is even, then add the `a` factor
+  # In any case, multiple the a factor by twice the number of rows
+
+  y0 + (col %% 2) * a + 2 * row * a
 }
 
-.calc_x <- function(i, j, s, x0=0) {
-  x0 + j * ((3 * s) / 2)
+.calc_x <- function(row, col, radius, x0 = 0) {
+  x0 + col * ((3 * radius) / 2)
 }
